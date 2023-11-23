d39a44b4b0ea92ddfbf2d358a9b5637438f8584c
xhe <xhe383@gatech.edu>
2023-11-23 15:26:27 -0500

    funnelPlot+navBar

---
+++ src/charts/constants.js
@@ -0,0 +1,30 @@
+// constants.js
+export const funnelChartData = [
+    {
+        id: '5',
+        value: 29,
+        label: '5 stars',
+    },
+    {
+        id: '4',
+        value: 846,
+        label: '4 stars',
+    },
+    {
+        id: '3',
+        value: 169,
+        label: '3 stars',
+    },
+    {
+        id: '2',
+        value: 12,
+        label: '2 stars',
+    },
+    {
+        id: '1',
+        value: 3,
+        label: '1 stars',
+    },
+];
+
+
