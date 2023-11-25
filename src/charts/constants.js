55de0ba023f485780d7eb28b7f4da11f15d40c98
Stella <stellazhu334@gmail.com>
2023-11-24 20:05:05 -0500

    update data

--- src/charts/constants.js
+++ src/charts/constants.js
@@ -2,27 +2,27 @@
 export const funnelChartData = [
     {
         id: '5',
-        value: 29,
+        value: 309,
         label: '5 stars',
     },
     {
         id: '4',
-        value: 846,
+        value: 675,
         label: '4 stars',
     },
     {
         id: '3',
-        value: 169,
+        value: 66,
         label: '3 stars',
     },
     {
         id: '2',
-        value: 12,
+        value: 8,
         label: '2 stars',
     },
     {
         id: '1',
-        value: 3,
+        value: 2,
         label: '1 stars',
     },
 ];
